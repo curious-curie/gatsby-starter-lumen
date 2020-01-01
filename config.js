3968cf52391982d6df59b42427ef5682e35913d1
curious-curie <curious.curieyoo@gmail.com>
2020-01-02 00:34:15 +0900

    feat: add disqus

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 'use strict';
 
 module.exports = {
-  url: 'https://lumen.netlify.com',
+  url: 'https://travelflan-techblog.netlify.com/',
   pathPrefix: '/',
   title: 'TravelFlan Tech Blog',
   subtitle: '트래블플랜 기술블로그',
   copyright: '© All rights reserved.',
-  disqusShortname: '',
+  disqusShortname: 'TravelFlan',
   postsPerPage: 4,
   googleAnalyticsId: 'UA-73379983-2',
   useKatex: false,
